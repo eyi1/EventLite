7590b2200ae8b9418962863118ff48a7d65ce1b9
eyi1 <ej7729@gmail.com>
2018-09-18 20:54:40 -0400

    add create in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
-    #before_action 
+    #before_action :find_user
     def new
         @user = User.new
         render :new
     end
 
     def create
-        @user = User.new(name: params[:name])
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user), notice: "Welcome to Event Builder- click create event to get started!"
+        else
+            render :new
+        end
     end
+
+    private
+
+    def user_params
+        @user = User.find_by(params[:id])
+    end
+
+    def find_user
+        params.require(:user).permit(
+            :name,
+            :username,
+            :email,
+            :password
+        )
+    end
+
 end
