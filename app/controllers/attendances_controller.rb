8c06834680828779485e4b797cc1c8f89fd3c5f4
eyi1 <ej7729@gmail.com>
2018-10-10 22:58:45 -0400

    add comment params

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,7 +1,7 @@
 class AttendancesController < ApplicationController
 
     def new      
-        @attendance = Attendance.create(user_id: params[:user_id], event_id: params[:event_id])
-        redirect_to user_path(@attendance.user)
+        @attendance = Attendance.create(user_id: params[:user_id], event_id: params[:event_id], comment: params[:comment])
+        redirect_to event_path(@attendance.event)
     end
 end
