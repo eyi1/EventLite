6def8c817e04278ced87c4e06ea6011b245c8729
eyi1 <ej7729@gmail.com>
2018-12-09 17:18:05 -0500

    update validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,15 +1,23 @@
 class Event < ApplicationRecord
     has_many :attendances
     has_many :users, :through => :attendances
-    #accepts_nested_attributes_for :users
+
+    validates :title, presence: true
+    validates :location, presence: true
+    validates :starts, presence: true
+    validates :ends, presence: true
+    validates :description, presence: true
 
     def self.from_today
-        #self.where(:event)
-        self.where("created_at >=?", Time.zone.today.beginning_of_day)
+        self.where("starts >=?", Time.zone.today.beginning_of_day)
     end
  
     def self.old_events
-         self.where("created_at <?", Time.zone.today.beginning_of_day)
+         self.where("starts <?", Time.zone.today.beginning_of_day)
+    end
+
+    def rsvp(event)
+        binding.pry
     end
 
     # def start_date
