0fbbfa51ee23bdddbd58b054d3d81b0e1ef56641
eyi1 <ej7729@gmail.com>
2018-12-13 12:53:20 -0500

    add id attribute and association to user

---
+++ app/serializers/attendance_serializer.rb
@@ -0,0 +1,6 @@
+class AttendanceSerializer < ActiveModel::Serializer
+    attributes :id
+    
+    belongs_to :user, serializer: AttendanceUserSerializer
+    #belongs_to :event, serializer: AttendanceEventSerializer
+  end
