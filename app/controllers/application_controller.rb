333f1e8dc540605ec054d026db1bae66e1532213
eyi1 <ej7729@gmail.com>
2018-12-14 11:49:20 -0500

    add and comment out code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :current_user
     before_action :authenticate_user, except: [:new, :create, :home]
-
+    
     def logged_in?
         !!current_user
     end
