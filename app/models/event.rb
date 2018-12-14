470b01fc61cd167bcd8e47015b0cee291fe5deb4
eyi1 <ej7729@gmail.com>
2018-12-14 16:53:22 -0500

    add to_next_event method

--- app/models/event.rb
+++ app/models/event.rb
@@ -12,11 +12,21 @@ class Event < ApplicationRecord
     validates :description, presence: true
 
     def self.from_today
-        self.where("starts >=?", Time.zone.today.beginning_of_day)
+        self.where("starts >= ?", Time.zone.today.beginning_of_day)
     end
  
     def self.old_events
-         self.where("starts <?", Time.zone.today.beginning_of_day)
+         self.where("starts < ?", Time.zone.today.beginning_of_day)
+    end
+
+    def to_next_event
+        event = Event.where("id > ?", id).first
+        
+        if event
+            event
+        else
+            Event.first
+        end
     end
 
     def rsvp(event)
