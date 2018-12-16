80032db7dcceb4e92bda92d8030ca7aa1fcdd80f
eyi1 <ej7729@gmail.com>
2018-12-16 09:23:48 -0500

    updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_131908) do
+ActiveRecord::Schema.define(version: 2018_12_16_135121) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 2018_10_09_131908) do
     t.string "comment"
     t.index ["event_id"], name: "index_attendances_on_event_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "event_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "events", force: :cascade do |t|
