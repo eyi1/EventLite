d2f7313382d7f0a9d5674b52366e65e4de652c25
eyi1 <ej7729@gmail.com>
2018-12-19 10:22:42 -0500

    submit comment form using ajax

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,10 +1,26 @@
-$(function(){
-    $("a.load_comments").on("click", function(e){
-        e.preventDefault();
-        $.get(this.href)
-        .success(function(res){
-            $("div.comments").html(res) 
+$(document).on('turbolinks:load', function(){
+    $("#new_comment").on("submit", function(){
+        //allows form to submit again
+        // e.preventDefault() DOES NOT allow form to submit again
+        let values = $(this).serialize();
+        let posting = $.post(this.action, values);
+        
+        posting.done(function(res){
+            $('#comment_content').val("")
+            $(".comments").append(res)            
         })
+        return false;
+    })        
+})
+
+//load comments on event show page
+// $(function(){
+//     $("a.load_comments").on("click", function(e){
+//         e.preventDefault();
+//         $.get(this.href)
+//         .done(function(res){
+//             $("div.comments").text(res) 
+//         })
 
         //low-level
         // $.ajax({
@@ -16,5 +32,5 @@ $(function(){
         // }).error(function(error){
         //     alert(error)
         // })
-    })
-})
+//     })
+// })
