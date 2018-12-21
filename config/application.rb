6fbe5be45785e87d9508a1e6af4e76d7ef85b11e
eyi1 <ej7729@gmail.com>
2018-12-21 14:55:11 -0500

    try to add boostrap-websockets and asset path

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module BuildEvent
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'bootstrap-sass-official', 'assets', 'fonts')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
